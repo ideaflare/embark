36341e8204a7cac5234635f6e5c1f2ef20019844
ubrgw <ekwilnetskaakspeel@gmail.com>
2015-03-02 23:46:01 +0200

    Added example demo code

---
+++ TestClient/Demo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClient.IO;
+
+namespace TestClient
+{
+    /// <summary>
+    /// Simple code example to demonstrate how to use embark
+    /// </summary>
+    class Demo
+    {
+        void SimpleDemo()
+        {
+            // arrange some guinea pig
+            var pet = new Sheep { Name = "Fluffy", FavouriteIceCream = IceCream.Vanilla };
+            var tag = "sheep";
+            
+            // save data locally
+            var client = new Embark.Client(@"C:\MyTemp\Embark");
+
+            // or over a network
+            // var client = new Embark.Client("127.0.0.1"), 80);// Not implemented, yet..
+
+            var io = client.IOChannel;
+
+            // insert
+            long id = io.Insert(tag, pet);
+
+            // get
+            Sheep fluffy = io.Get<Sheep>(tag, id);
+
+            // update
+            fluffy.FavouriteIceCream = IceCream.Strawberry;
+            
+            bool hasSheepUpdated = io.Update(tag, id, fluffy);
+
+            // delete
+            bool hasSheepVanished = io.Delete(tag, id);
+        }
+    }
+}
