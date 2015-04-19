9d76e2cf3695f0ffc0a5c31ef83ee3c746d42267
ubrgw <ekwilnetskaakspeel@gmail.com>
2015-04-19 11:16:07 +0200

    Add Wrapper tests

---
+++ TestClient/TestWrapper.cs
@@ -0,0 +1,61 @@
+﻿using Embark.Conversion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClient.IO.TestData;
+using TestClient.TestData;
+
+namespace TestClient
+{
+    [TestClass]
+    public class TestWrapper
+    {
+        [TestMethod]
+        public void GetWrapper_ReturnsDocument()
+        {
+            // arrange
+            var saved = Animals.GetTestSheep();
+
+            var io = Cache.localClient.GetCollection<Sheep>("wrapSelect");
+
+            long id = io.Insert(saved);
+
+            // act
+            DocumentWrapper<Sheep> wrapper = io.GetWrapper(id);
+
+            var loaded = wrapper.Value;
+
+            // assert
+            Assert.AreEqual(wrapper.ID, id);
+            Assert.AreEqual(saved.Name, loaded.Name);
+            Assert.AreEqual(saved.Age, loaded.Age);
+            Assert.AreEqual(saved.FavouriteIceCream, loaded.FavouriteIceCream);   
+        }
+
+        
+        [TestMethod]
+        public void WrapperUpdate_CommitsChanges()
+        {
+            // arrange
+            var saved = Animals.GetTestSheep();
+            var io = Cache.localClient.GetCollection<Sheep>("wrapUpdate");
+            long id = io.Insert(saved);
+            DocumentWrapper<Sheep> wrapper = io.GetWrapper(id);
+
+            // act
+            var olderAge = saved.Age + 1;
+            wrapper.Value.Age = olderAge;
+            wrapper.Update();
+
+            var agedSheep = io.Get(id);
+
+            // assert
+            Assert.AreEqual(wrapper.ID, id);
+
+            Assert.AreEqual(olderAge, agedSheep.Age);
+        }
+    }
+}
