c8a904a661efb1e678724e9575e9b556f6ba325c
ubrgw <ekwilnetskaakspeel@gmail.com>
2015-04-26 20:06:24 +0200

    Removed unused code

--- Embark/Storage/WebServiceRepository.cs
+++ Embark/Storage/WebServiceRepository.cs
@@ -64,44 +64,6 @@ namespace Embark.Storage
         IEnumerable<DataEnvelope> ITextDataStore.GetBetween(string tag, string startRange, string endRange)
         {
             return CallRemoteDatastore<IEnumerable<DataEnvelope>>((store) => store.GetBetween(tag, startRange, endRange));
-        }
-        
-        //private HttpClient GetClient()
-        //{
-        //    var client = new HttpClient();
-
-        //    client.BaseAddress = serviceUri;
-        //    client.DefaultRequestHeaders.Accept.Clear();
-        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-        //    //// HTTP GET
-        //    //HttpResponseMessage response = await client.GetAsync("api/products/1");
-        //    //if (response.IsSuccessStatusCode)
-        //    //{
-        //    //    Product product = await response.Content.ReadAsAsync<Product>();
-        //    //    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
-        //    //}
-
-        //    //// HTTP POST
-        //    //var gizmo = new Product() { Name = "Gizmo", Price = 100, Category = "Widget" };
-        //    //response = await client.PostAsJsonAsync("api/products", gizmo);
-
-        //    //if (response.IsSuccessStatusCode)
-        //    //{
-        //    //    Uri gizmoUrl = response.Headers.Location;
-
-        //    //    // HTTP PUT
-        //    //    gizmo.Price = 80;   // Update price
-        //    //    response = await client.PutAsJsonAsync(gizmoUrl, gizmo);
-
-        //    //    // HTTP DELETE
-        //    //    response = await client.DeleteAsync(gizmoUrl);
-        //    //}
-
-        //    return client;
-        //}
-
-
-        
+        }        
     }
 }
