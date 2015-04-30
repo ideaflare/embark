166cad082553f1e32cb9922636a997061544d0db
ubrgw <ekwilnetskaakspeel@gmail.com>
2015-04-30 20:52:47 +0200

    Added generic ActionCommand test

---
+++ TestClient/DesignPatterns/MVVMCommandTests.cs
@@ -0,0 +1,80 @@
+﻿using Embark.DesignPatterns.MVVM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestClient.DesignPatterns
+{
+    [TestClass]
+    public class MVVMCommandTests
+    {
+        [TestMethod]
+        public void ActionCommands_FireAsExpected()
+        {
+            //Arrange
+            var fireBasic = new TestFire();
+            var fireParameter = new TestFire();
+
+            ICommand basicCommand = new ActionCommand(fireBasic.ExecuteNone, fireBasic.CanExecute);
+            ICommand paramaterCommand = new ActionCommand<int>(fireParameter.ExecuteParam, fireParameter.CanExecuteParam);
+
+            //Act
+            basicCommand.CanExecute(null);
+            basicCommand.Execute(null);
+            paramaterCommand.CanExecute(1);
+            paramaterCommand.Execute(2);
+
+            //Assert
+            Assert.IsTrue(fireBasic.ExecuteFired);
+            Assert.IsTrue(fireBasic.TestFired);
+
+            Assert.IsTrue(fireParameter.ExecuteFired);
+            Assert.IsTrue(fireParameter.TestFired);
+            Assert.AreEqual(2, fireParameter.ObjectsPassedIn.Count);
+            Assert.AreEqual(3, fireParameter.ObjectsPassedIn.Sum());
+        }
+
+
+    }
+
+    public class TestFire
+    {
+        public TestFire()
+        {
+            ExecuteFired = false;
+            TestFired = false;
+        }
+
+        public bool ExecuteFired { get; private set; }
+        public bool TestFired { get; set; }
+
+        public List<int> ObjectsPassedIn = new List<int>();
+
+        public void ExecuteNone()
+        {
+            ExecuteFired = true;
+        }
+
+        public bool CanExecute()
+        {
+            TestFired = true;
+            return true;
+        }
+
+        public void ExecuteParam(int param)
+        {
+            ObjectsPassedIn.Add(param);
+            ExecuteNone();
+        }
+
+        public bool CanExecuteParam(int param)
+        {
+            ObjectsPassedIn.Add(param);
+            return CanExecute();
+        }
+    }
+}
